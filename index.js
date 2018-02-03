bbae8b278787c71295bb781fa6f0df16cfda4f92
Rice <rice9547@gmail.com>
2018-02-04 06:43:41 +0800

    Create scene file

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ import Register1 from './app/scene/register1';
 import Register2 from './app/scene/register2';
 import Register3 from './app/scene/register3';
 import Main from './app/scene/main';
+import Profile from './app/scene/profile';
+import Qrcode from './app/scene/qrcode';
+import Sailing from './app/scene/sailing';
+import Statistic from './app/scene/statistics';
 
 const App = StackNavigator({
 	Main: { screen: Main },
@@ -16,6 +20,10 @@ const App = StackNavigator({
 	Register1: { screen: Register1 },
 	Register2: { screen: Register2 },
 	Register3: { screen: Register3 },
+	Profile: { screen: Profile },
+	Qrcode: { screen: Qrcode },
+	Sailing: { screen: Sailing },
+	Statistic: { screen: Statistic }
 });
 
 AppRegistry.registerComponent('fishackthon', () => App);
